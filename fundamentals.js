e7d575e1ce99c84add4477864882307fb32c32f6
Rami Taha <ramisn3@gmail.com>
2017-07-18 14:35:12 -0400

    checkpoint

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,35 @@
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
 
-
+colors = ["red", "blue", "green"]
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
-
+last = colors[2]
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-
+favoriteColors = []
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
+for ( i=0; i < colors.length; i++) {
+  favoriteColors.concat.colors
+console.log(favoriteColors)
 
-
-
+}
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+instructor = [{
+  name: 'rami',
+  course: 'jscript',
+  location: 'dc'
+}]
 
 
 // #6: Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor.['facial-hair']= true
--- hof.js
+++ hof.js
@@ -10,10 +10,16 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peoplesAges`.
 // Type your solution immediately below this line:
-
-
-
-
+var peoplesAges = []
+var ages = people[i]
+function getAge (people){
+  var ages = people[i]
+  for (let i = 0; i > people.length; i++) {
+  peoplesAges.push(ages.age)
+  return peoplesAges
+}
+}
+map(people.getAge())
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are older than 35. Assign the returned array
 // to a variable called `peopleOlderThan35`.
